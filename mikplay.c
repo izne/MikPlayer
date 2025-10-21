90cbf89ff26a3fb44cdffc7d69f94f7148c810ee
Dimitar Angelov <funkamateur@gmail.com>
2025-10-21 02:01:26 +0200

    Fixed max_voices not being used in init.

--- mikplay.c
+++ mikplay.c
@@ -32,9 +32,9 @@
 #include <sys/stat.h>
 #include <mikmod.h>
 
-#define MEM_LOAD_THRESHOLD 6291456  // load to memory if smaller than 6MB
+#define MEM_LOAD_THRESHOLD 6291456  // load to memory if smaller than 6 megs
 #define VER_MAJ 0
-#define VER_MIN 2
+#define VER_MIN 3
 #define VER_STR "retrohw"
 
 
@@ -47,8 +47,10 @@ void update_status(MODULE *module, int volume) {
     int num_voices = module->numvoices;
     
     // volumeto
-    for (i = 0; i < num_voices; i++) {
-        if (!Voice_Stopped(i)) {
+    for (i = 0; i < num_voices; i++)
+    {
+        if (!Voice_Stopped(i))
+        {
             int vol = Voice_GetVolume(i);
             active_channels++;
             total_volume += vol;
@@ -72,7 +74,8 @@ void update_status(MODULE *module, int volume) {
            num_voices);
     
     // VU bars
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++)
+    {
         if (i < vu_level) printf("=");
         else printf(" ");
     }
@@ -92,14 +95,15 @@ int main(int argc, char *argv[])
     int use_memory_load = 0;
     int max_voices = 64;
     int mix_freq = 22050;
-    int low_cpu_mode = 0;
+    int force_mono = 0;
+    char *s_profile = "486";
     int i;
     
     if (argc < 2) {
         printf("\nUsage: mikplay <module_file> [options]\n");
         printf("Options:\n");
         printf("  -386      386 mode: 11kHz, mono, 8 voices, slow display\n");
-        printf("  -486      486 mode: 22kHz, stereo, 16 voices\n");
+        printf("  -486      Slow 486 mode: 22kHz, stereo, 16 voices\n");
         printf("  -hifi     Hi-Fi mode: 44kHz, stereo, 64 voices (default: 22kHz)\n");
         printf("  -mono     Force mono output\n");
         printf("  -v<num>   Set max voices (default: 64)\n");
@@ -108,38 +112,43 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-
     printf("\nMikPlayer, ver.%d.%d-%s\n(c) 2025 Dimitar Angelov\n\n", VER_MAJ, VER_MIN, VER_STR);
 
     // Parse command line options
     for (i = 2; i < argc; i++)
     {
-        if (strcmp(argv[i], "-386") == 0) {
+        if (strcmp(argv[i], "-386") == 0)
+        {
             mix_freq = 11025;
             max_voices = 8;
-            low_cpu_mode = 1;
+            force_mono = 1;
             update_interval = CLOCKS_PER_SEC; // once per second
-            printf("386 mode enabled: 11kHz mono, 8 voices\n");
+            s_profile = "386";
         }
-        else if (strcmp(argv[i], "-486") == 0) {
+        else if (strcmp(argv[i], "-486") == 0)
+        {
             mix_freq = 22050;
             max_voices = 16;
-            printf("Slow 486 mode enabled: 22kHz stereo, 16 voices\n");
+            s_profile = "slow 486";
         }
-        else if (strcmp(argv[i], "-hifi") == 0) {
+        else if (strcmp(argv[i], "-hifi") == 0)
+        {
             mix_freq = 44100;
             max_voices = 64;
-            printf("Hi-Fi mode enabled: 44kHz stereo, 64 voices\n");
+            s_profile = "hifi";
         }
-        else if (strcmp(argv[i], "-mono") == 0) {
-            low_cpu_mode = 1;
-            printf("Mono output enabled\n");
+        else if (strcmp(argv[i], "-mono") == 0)
+        {
+            force_mono = 1;
+            printf("Forced mono output\n");
         }
-        else if (argv[i][0] == '-' && argv[i][1] == 'v') {
+        else if (argv[i][0] == '-' && argv[i][1] == 'v')
+        {
             max_voices = atoi(argv[i] + 2);
             printf("Max voices set to: %d\n", max_voices);
         }
-        else if (argv[i][0] == '-' && argv[i][1] == 'f') {
+        else if (argv[i][0] == '-' && argv[i][1] == 'f')
+        {
             mix_freq = atoi(argv[i] + 2);
             printf("Mix frequency set to: %d Hz\n", mix_freq);
         }
@@ -158,15 +167,17 @@ int main(int argc, char *argv[])
     // init output modes
     md_mode = DMODE_SOFT_MUSIC;
     md_mixfreq = mix_freq;
-    if (low_cpu_mode) md_mode &= ~DMODE_STEREO; // disable stereo for 386
+    if (force_mono) md_mode &= ~DMODE_STEREO;
     else md_mode |= DMODE_STEREO;
     
-    printf("Audio output: %dHz, %s, %d voices max\n",
-           mix_freq,
-           low_cpu_mode ? "mono" : "stereo",
-           max_voices);
+    printf("Audio output (%s): %dHz, %s, %d voices max\n",
+        s_profile,
+        mix_freq,
+        force_mono ? "mono" : "stereo",
+        max_voices);
 
-    if (MikMod_Init("")) {
+    if (MikMod_Init(""))
+    {
         printf("Could not initialize MikMod: %s\n", MikMod_strerror(MikMod_errno));
         return 1;
     }
@@ -188,21 +199,24 @@ int main(int argc, char *argv[])
         printf("Loading %s to memory (%.2f KB): ...\n", filename, file_size / 1024.0);
         
         fp = fopen(filename, "rb");
-        if (!fp) {
+        if (!fp)
+        {
             printf("Could not open file for reading\n");
             MikMod_Exit();
             return 1;
         }
         
         buffer = (char *)malloc(file_size);
-        if (!buffer) {
+        if (!buffer)
+        {
             printf("Could not allocate memory for module\n");
             fclose(fp);
             MikMod_Exit();
             return 1;
         }
         
-        if (fread(buffer, 1, file_size, fp) != file_size) {
+        if (fread(buffer, 1, file_size, fp) != file_size)
+        {
             printf("Error reading file\n");
             free(buffer);
             fclose(fp);
@@ -211,26 +225,26 @@ int main(int argc, char *argv[])
         }
         fclose(fp);
         
-        module = Player_LoadMem(buffer, file_size, 64, 0);
+        module = Player_LoadMem(buffer, file_size, max_voices, 0);
         free(buffer);
     } 
     else 
     {
         // Load chunks (streaming from file)
         printf("Streaming %s from disk: ...\n", filename);
-        module = Player_Load(filename, 64, 0);
+        module = Player_Load(filename, max_voices, 0);
     }
 
 
-    if (!module) {
+    if (!module)
+    {   
         printf("Could not load module: %s\n", MikMod_strerror(MikMod_errno));
         MikMod_Exit();
         return 1;
     }
     
-    if(module->comment) printf("Comment: %s\n", module->comment);
-    printf("Name: %s  Type: %s\n", module->songname, module->modtype);
-    printf("Channels: %d  Patterns: %d  Instruments: %d  Samples: %d\n", module->numchn, module->numpos, module->numins, module->numsmp);
+    // if(module->comment) printf("Comment: %s\n", module->comment);
+    printf("Type: %s\nChannels: %d, Patterns: %d, Instruments: %d, Samples: %d\n\nName: %s\n", module->modtype, module->numchn, module->numpos, module->numins, module->numsmp, module->songname);
     
     Player_Start(module);
     Player_SetVolume(current_volume);
@@ -239,7 +253,8 @@ int main(int argc, char *argv[])
     printf("                                                                            ");
     update_status(module, current_volume);
     
-    while (1) {
+    while (1)
+    {
         clock_t now = clock();
 
         MikMod_Update(); // update audio mixing
@@ -250,55 +265,57 @@ int main(int argc, char *argv[])
         }
         
         // key press handing
-        if (kbhit()) {
+        if (kbhit())
+        {
             int ch = getch();
             
-            if (ch == 27 || ch == 81 || ch == 113) { // ESC or Q or q
-                break;
-            }
-            else if (ch == ' ') { // space
+            if (ch == 27 || ch == 81 || ch == 113) break; // ESC or Q or q
+            else if (ch == ' ')
+            { // space
                 Player_TogglePause();
                 update_status(module, current_volume);
             }
-            else if (ch == '+' || ch == '=') {
+            else if (ch == '+' || ch == '=')
+            {
                 if (current_volume < 128) {
                     current_volume += 8;
                     Player_SetVolume(current_volume);
                     update_status(module, current_volume);
                 }
             }
-            else if (ch == '-' || ch == '_') {
+            else if (ch == '-' || ch == '_')
+            {
                 if (current_volume > 0) {
                     current_volume -= 8;
                     Player_SetVolume(current_volume);
                     update_status(module, current_volume);
                 }
             }
-            else if (ch == 0) { // extended key (arrows, etc.)
+            else if (ch == 0)
+            { // extended key (arrows, etc.)
                 ch = getch();
                 
                 if (ch == 75) { // left arrow - skip backward
-                    if (module->sngpos > 0) {
+                    if (module->sngpos > 0)
+                    {
                         Player_SetPosition(module->sngpos - 1);
                         update_status(module, current_volume);
                     }
                 }
                 else if (ch == 77) { // right arrow - skip forward
-                    if (module->sngpos < module->numpos - 1) {
+                    if (module->sngpos < module->numpos - 1)
+                    {
                         Player_SetPosition(module->sngpos + 1);
                         update_status(module, current_volume);
                     }
                 }
             }
         }
-        // eof
-        if (!Player_Active()) {
-            printf("\nFinished!\n");
-            break;
-        }
+        if (!Player_Active()) break;
     }
     
     // Cleanup
+    printf("\nEOF\n");
     Player_Stop();
     Player_Free(module);
     MikMod_Exit();
